947ac2619b25772f0d1f782400e5168a444fe33d
Alexei Ivanov <aaivanov23@gmail.com>
2022-04-15 19:47:51 +0300

    remove redundant variable

--- src/com/company/Markov.java
+++ src/com/company/Markov.java
@@ -8,8 +8,7 @@ public class Markov {
     static final int MAXGEN = 1000;
     public static void main(String[] args) throws IOException {
         Chain chain = new Chain();
-        int nwords = MAXGEN;
         chain.build(new FileInputStream("/Users/alexei/IdeaProjects/Java/Markov chain/src/com/company/data.txt"));
-        chain.generate(nwords);
+        chain.generate(MAXGEN);
     }
 }
